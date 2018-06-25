e3fecfd4500225ae337a202bb0b9ed445a537134
Florian Chlan <florian@chlan.net>
2018-06-25 14:22:12 +0200

    Added cross validation to kNN shapley calculation (WIP)

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate rusty_machine;
 mod data_loader;
 mod knn_shapley;
 
-use rusty_machine::prelude::{BaseMatrix, Vector};
+use rusty_machine::prelude::{BaseMatrix, BaseMatrixMut, Vector, Matrix};
 use data_loader::data_loader::{CSVLoader, DataLoader};
 use knn_shapley::knn_shapley::*;
 use rustlearn::cross_validation::{ShuffleSplit, CrossValidation};
@@ -21,22 +21,27 @@ fn main() {
     let X = csv_ldr.vecs_as_matrix(features);
     let y = csv_ldr.vec_as_vector(predictors);
 
-    let num_splits = 149;
+    let num_splits = 5;
     let test_percentage = 0.2;
 
-    
+    let mut split_shapleys = Matrix::zeros(num_splits, X.rows());
 
     //for (train_idx, test_idx) in ShuffleSplit::new(X.rows(), num_splits, test_percentage) {
-    for (train_idx, test_idx) in CrossValidation::new(X.rows(), num_splits) {
+    for (split_index, (train_idx, test_idx)) in CrossValidation::new(X.rows(), num_splits).enumerate() {
         let X_train = X.select_rows(&train_idx);
         let y_train = y.select(&train_idx);
         let X_test = X.select_rows(&test_idx);
         let y_test = y.select(&test_idx);
         
-        //let dist = distance_from_sample(&X_train, &Vector::new(X_test.get_row(0).unwrap()));
-        let shapleys = calculate_knn_shapleys(&X_train, &y_train, &X_test, &y_test, 10);
-        println!("{:?} {:?}", train_idx, test_idx);
-        println!("{:?}", shapleys);
-        break;
+        let shapleys = calculate_knn_shapleys(&X_train, &y_train, &X_test, &y_test, 10).into_vec();
+        
+        let mut row_slice = split_shapleys.get_row_mut(split_index).unwrap();
+        for (i, item) in train_idx.iter().enumerate() {
+            row_slice[*item] = shapleys[i];
+        }
+        // TODO Use indicator matrix to track which shapley values are set and which are just the default 0
+        // TODO Average over columns to get shapley value of each sample (avg only over indicated values)
+
     }
+    println!("{:?}", split_shapleys);
 }
